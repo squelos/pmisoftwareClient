b9970bac4202ea6cd9288d44a35709588ef8a204
Christopher Robert <osiris57@gmail.com>
2013-11-20 17:44:17 +0100

    Binding Test added, corrections to the model

--- Tcp/TcpModernUI/Pages/players/creation.xaml.cs
+++ Tcp/TcpModernUI/Pages/players/creation.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TcpDataModel;
 
 namespace TcpModernUI.Pages.players
 {
@@ -20,9 +21,36 @@ namespace TcpModernUI.Pages.players
     /// </summary>
     public partial class creation : UserControl
     {
+        public TcpDataModel.Player currentPlayer;
+        public entityContainer container = new TcpDataModel.entityContainer();
+
         public creation()
         {
             InitializeComponent();
+            currentPlayer = new Player();    
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //save
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            //cancel
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            // Do not load your data at design time.
+            // if (!System.ComponentModel.DesignerProperties.GetIsInDesignMode(this))
+            // {
+            // 	//Load your data here and assign the result to the CollectionViewSource.
+            // 	System.Windows.Data.CollectionViewSource myCollectionViewSource = (System.Windows.Data.CollectionViewSource)this.Resources["Resource Key for CollectionViewSource"];
+            // 	myCollectionViewSource.Source = your data
+            // }
         }
     }
 }
