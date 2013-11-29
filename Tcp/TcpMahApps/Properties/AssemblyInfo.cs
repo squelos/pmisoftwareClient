6dca8531c9896ffe24134730d641f5e6d97eb12b
Christopher Robert <osiris57@gmail.com>
2013-11-29 11:53:42 +0100

    Breaking all

--- Tcp/TcpMahApps/Properties/AssemblyInfo.cs
+++ Tcp/TcpMahApps/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Test")]
+[assembly: AssemblyTitle("TcpMahApps")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Test")]
+[assembly: AssemblyProduct("TcpMahApps")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
