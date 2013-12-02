e04f50f4f2fd63619f0a33d81572c23e761087f7
squelos <osiris57@gmail.com>
2013-12-02 23:53:11 +0100

    take a break

--- Tcp/TcpModernUI/Pages/badges/creationBadges.xaml.cs
+++ Tcp/TcpModernUI/Pages/badges/creationBadges.xaml.cs
@@ -25,5 +25,10 @@ namespace TcpModernUI.Pages.badges
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
---
+++ Tcp/TcpModernUI/Pages/badges/popup.xaml.cs
@@ -0,0 +1,16 @@
+﻿using MahApps.Metro.Controls;
+using TcpModernUI.Utility;
+
+namespace TcpModernUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class popup
+    {
+        public popup()
+        {
+            
+        }
+    }
+}
