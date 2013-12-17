3bab19445e429315eaf627b43ba0ae1005ccbb36
Christopher Robert <osiris57@gmail.com>
2013-12-17 16:25:09 +0100

    BEtter

--- Tcp/TcpModernUI/ViewModel/PlayersViewModel.cs
+++ Tcp/TcpModernUI/ViewModel/PlayersViewModel.cs
@@ -71,26 +71,26 @@ namespace TcpModernUI.ViewModel
                                           };
 
 
-            Task t = new Task(() =>
-            {
-                foreach (Player p in Players)
-                {
-                    if (p.Payment.Count == 0)
-                    {
-                        CustomDispatcher.Instance.BeginInvoke(() => Unpaid.Add(p));
-                    }
-                    else
-                    {
-                        if (
-                            !p.Payment.Any(
-                                payment =>
-                                    payment.Semester.Any(semester => semester.end > DateTime.Now)))
-                        {
-                            CustomDispatcher.Instance.BeginInvoke(() => Unpaid.Add(p));
-                        }
-                    }
-                }
-            });
+            var t = new Task(() =>
+                              {
+                                  foreach (Player p in Players)
+                                  {
+                                      if (!p.Payment.Any())
+                                      {
+                                          CustomDispatcher.Instance.BeginInvoke(() => Unpaid.Add(p));
+                                      }
+                                      else
+                                      {
+                                          if (
+                                              !p.Payment.Any(
+                                                  payment =>
+                                                      payment.Semester.Any(semester => semester.end > DateTime.Now)))
+                                          {
+                                              CustomDispatcher.Instance.BeginInvoke(() => Unpaid.Add(p));
+                                          }
+                                      }
+                                  }
+                              });
 
             t.Start();
 
