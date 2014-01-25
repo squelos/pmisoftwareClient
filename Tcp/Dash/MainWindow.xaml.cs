f4fdb496a75d4da768a7b4556aade2479dd3b5fb
squelos <osiris57@gmail.com>
2014-01-25 22:48:28 +0100

    Still working

--- Tcp/Dash/MainWindow.xaml.cs
+++ Tcp/Dash/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Dash.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,11 +19,35 @@ namespace Dash
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow 
+    public partial class MainWindow
     {
+        private MainViewModel _mainViewModel;
         public MainWindow()
         {
             InitializeComponent();
+            //InkInputHelper.DisableWPFTabletSupport();
+        }
+
+        private void MetroWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            //_mainViewModel.BadgeScanner.Dispose();
+        }
+
+        private void MetroWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            var dataContext = DataContext;
+            var mainViewModel = dataContext as MainViewModel;
+            if (mainViewModel != null)
+            {
+                _mainViewModel = mainViewModel;
+            }
+
+            //System.Windows.Automation.AutomationElement asForm =
+            //   System.Windows.Automation.AutomationElement.FromHandle(new WindowInteropHelper(this).Handle);
+
+            //// Windows 8 API to enable touch keyboard to monitor for focus tracking in this WPF application
+            //InputPanelConfigurationLib.InputPanelConfiguration inputPanelConfig = new InputPanelConfigurationLib.InputPanelConfiguration();
+            //inputPanelConfig.EnableFocusTracking();
         }
     }
 }
