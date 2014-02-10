d77e349d853c67fc94200d0b0a6a664c22ddd5e8
luke.marlin <luke.marlin@viacesi.fr>
2014-02-10 22:39:52 +0100

    Added TcpGridRow style with both MahApps style plus ours.

---
+++ Tcp/TcpGA/MergedStylesExtension.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Markup;
+
+namespace TcpGA
+{
+    [MarkupExtensionReturnType(typeof(Style))]
+    public class MergedStylesExtension : MarkupExtension
+    {
+        public Style BasedOn { get; set; }
+        public Style MergeStyle { get; set; }
+
+        public override object ProvideValue(IServiceProvider
+                                            serviceProvider)
+        {
+            if (null == MergeStyle)
+                return BasedOn;
+
+            Style newStyle = new Style(BasedOn.TargetType,
+                                       BasedOn);
+
+            MergeWithStyle(newStyle, MergeStyle);
+
+            return newStyle;
+        }
+
+        private static void MergeWithStyle(Style style,
+                                           Style mergeStyle)
+        {
+            // Recursively merge with any Styles this Style
+            // might be BasedOn.
+            if (mergeStyle.BasedOn != null)
+            {
+                MergeWithStyle(style, mergeStyle.BasedOn);
+            }
+
+            // Merge the Setters...
+            foreach (var setter in mergeStyle.Setters)
+                style.Setters.Add(setter);
+
+            // Merge the Triggers...
+            foreach (var trigger in mergeStyle.Triggers)
+                style.Triggers.Add(trigger);
+        }
+    }
+}
